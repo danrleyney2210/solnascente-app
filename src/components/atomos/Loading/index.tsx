f6cb2b3df76ea877f205b12e30fb2959b687df74
Danrley Alves <ney@Danrleys-MacBook-Pro.local>
2023-11-09 15:18:44 -0300

    feat: novos ajustes

--- src/components/atomos/Loading/index.tsx
+++ src/components/atomos/Loading/index.tsx
@@ -1,17 +1,24 @@
 import * as S from './styles'
-// import Lottie from 'react-lottie';
+import Lottie from 'react-lottie';
 
 
 export function Loading() {
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: '',
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
   return (
     <S.Wrapper>
-      {/* <Lottie options={defaultOptions}
-              height={400}
-              width={400}
-              isStopped={this.state.isStopped}
-              isPaused={this.state.isPaused}/> */}
+      <Lottie options={defaultOptions}
+        height={400}
+        width={400}
+      />
     </S.Wrapper>
   )
 }
